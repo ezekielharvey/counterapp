ee483cf2ff6db8ac5d50266027011fe5f8f0bec1
ezekielharvey <zekeharvey@outlook.com>
2022-05-19 13:08:22 -0400

    Created new function for props

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    render() {
-        return (
-            <nav
-                class="navbar bg-light"
+
+const NavBar = ({ totalCounters }) => {
+    <nav
+        class="navbar bg-light"
+    >
+        <div
+            class="container-fluid"
+        >
+            <a
+                class="navbar-brand"
+                href="#"
             >
-                <div
-                    class="container-fluid"
-                >
-                    <a
-                        class="navbar-brand"
-                        href="#"
-                    >
-                        Navbar <span className="badge-pill badge-secondary">{this.props.totalCounters}</span>
-                    </a>
-                </div>
-            </nav>
-        );
-    }
+                Navbar{''} <span className="badge-pill badge-secondary">{totalCounters}</span>
+            </a>
+        </div>
+    </nav>
+
 }
 
 export default NavBar;
