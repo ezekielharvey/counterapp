fa06aca00ff1c12610469ba947f720bbb9d45b6c
ezekielharvey <zekeharvey@outlook.com>
2022-05-19 12:22:24 -0400

    Added Navigation Bar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <nav
+                class="navbar bg-light"
+            >
+                <div
+                    class="container-fluid"
+                >
+                    <a
+                        class="navbar-brand"
+                        href="#"
+                    >
+                        Navbar
+                    </a>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default NavBar;
