8fe3e6d3736fe5ff3329bbef6ac92ab23e0c023b
ezekielharvey <zekeharvey@outlook.com>
2022-05-18 21:40:00 -0400

    Added Badge Functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,18 +3,38 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
+        imageUrl: 'https://picsum.photos/200'
     };
+
+    styles = {
+        fontSize: 10,
+        fontWeight: 'normal',
+    }
+
     render() {
+        let classes = 'badge m-2 badge-';
+        classes += this.state.count === 0 ? 'warning' : 'primary';
+
         return (
             <React.Fragment>
-                <span>
-                    Hello World!
+                <span
+                    style={this.styles}
+                    className={classes}
+                >
+                    {this.formatCount()}
                 </span>
-                <button>
+                <button
+                    className="btn btn-secondary btn-sm"
+                >
                     Increment
                 </button>
             </React.Fragment>
         );
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
 
