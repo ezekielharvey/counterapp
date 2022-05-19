8d01483a5b613adc5d5f24e6fb476d1931812796
ezekielharvey <zekeharvey@outlook.com>
2022-05-19 12:09:08 -0400

    Added Reset Button Functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        value: this.props.value,
+        value: this.props.counter.value,
         tags: ['tag1', 'tag2', 'tag3'],
         imageUrl: 'https://picsum.photos/200'
     };
@@ -42,13 +42,13 @@ class Counter extends Component {
 
                     </span>
                     <button
-                        onClick={() => this.handleIncrement()}
+                        onClick={() => this.props.onIncrement(this.props.counter)}
                         className="btn btn-secondary btn-sm"
                     >
                         Increment
                     </button>
                     <button
-                        onClick={() => this.props.onDelete(this.props.id)}
+                        onClick={() => this.props.onDelete(this.props.counter.id)}
                         className="btn btn-danger btn-sm m-2"
                     >
                         Delete
@@ -64,12 +64,12 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = 'badge m-2 badge-';
-        classes += this.state.value === 0 ? 'warning' : 'primary';
+        classes += this.props.counter.value === 0 ? 'warning' : 'primary';
         return classes;
     }
 
     formatCount() {
-        const { value } = this.state;
+        const { value } = this.props.counter;
         return value === 0 ? 'Zero' : value;
     }
 }
