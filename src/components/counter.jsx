1c093b5ae06a1e89a89ea7a9827d0d56995885a0
ezekielharvey <zekeharvey@outlook.com>
2022-05-19 10:59:01 -0400

    Added Title for Individual Buttons

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
+        value: this.props.value,
         tags: ['tag1', 'tag2', 'tag3'],
         imageUrl: 'https://picsum.photos/200'
     };
@@ -27,43 +27,46 @@ class Counter extends Component {
 
     handleIncrement = product => {
         console.log(product);
-        this.setState({ count: this.state.count + 1 });
+        this.setState({ value: this.state.value + 1 });
     }
 
     render() {
         let classes = this.getBadgeClasses();
         return (
-            <div>
-                <span
-                    style={this.styles}
-                    className={classes}
-                >
-                    {this.formatCount()}
+            <React.Fragment>
+                <div>
+                    {this.props.children}
+                    <span
+                        style={this.styles}
+                        className={classes}
+                    >
+                        {this.formatCount()}
 
-                </span>
-                <button
-                    onClick={() => this.handleIncrement()}
-                    className="btn btn-secondary btn-sm"
-                >
-                    Increment
-                </button>
+                    </span>
+                    <button
+                        onClick={() => this.handleIncrement()}
+                        className="btn btn-secondary btn-sm"
+                    >
+                        Increment
+                    </button>
+                </div>
                 <div>
                     {this.state.tags.length === 0 && 'Please create a new tag!'}
                     {this.renderTags()}
                 </div>
-            </div>
+            </React.Fragment>
         );
     }
 
     getBadgeClasses() {
         let classes = 'badge m-2 badge-';
-        classes += this.state.count === 0 ? 'warning' : 'primary';
+        classes += this.state.value === 0 ? 'warning' : 'primary';
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
+        const { value } = this.state;
+        return value === 0 ? 'Zero' : value;
     }
 }
 
