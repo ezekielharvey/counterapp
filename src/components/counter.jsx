66a0ce1ffda64ec038cfc411d867cc4cdbd54711
ezekielharvey <zekeharvey@outlook.com>
2022-05-19 09:52:51 -0400

    Button Functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -12,6 +12,18 @@ class Counter extends Component {
         fontWeight: 'normal',
     }
 
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+        return <ul>
+            {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+        </ul>
+    }
+
+    handleIncrement() {
+        console.log('Increment Clicked');
+    }
+
     render() {
         let classes = this.getBadgeClasses();
         return (
@@ -21,15 +33,18 @@ class Counter extends Component {
                     className={classes}
                 >
                     {this.formatCount()}
+
                 </span>
                 <button
+                    onClick={this.handleIncrement}
                     className="btn btn-secondary btn-sm"
                 >
                     Increment
                 </button>
-                <ul>
-                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                </ul>
+                <div>
+                    {this.state.tags.length === 0 && 'Please create a new tag!'}
+                    {this.renderTags()}
+                </div>
             </React.Fragment>
         );
     }
